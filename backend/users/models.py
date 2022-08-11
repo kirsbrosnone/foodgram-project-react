87b305a78cbef777fe3a117ab484b1b93a3f515d
roman <romat42ru@gmail.com>
2022-08-11 23:24:28 +0700

    user model added

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,8 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class MyUser(AbstractUser):
+class CustomUser(AbstractUser):
     """Кастомный класс пользователя."""
+
     email = models.EmailField(
         max_length=254, unique=True, blank=False, null=False,
         help_text='Адрес электронной почты',
@@ -21,14 +22,39 @@ class MyUser(AbstractUser):
         help_text='Фамилия'
     )
 
+    anon = 'anon'
+    user = 'user'
+    moderator = 'moderator'
+    admin = 'admin'
 
-class Subscribe(models.Model):
+    ROLE_CHOICES = [
+        (anon, 'Anonymous User'),
+        (user, 'Authenticated User'),
+        (moderator, 'Moderator'),
+        (admin, 'Administrator'),
+    ]
+
+    role = models.CharField(choices=ROLE_CHOICES, default=user, max_length=9)
+
+    def __str__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        if self.is_staff is True:
+            self.role = self.admin
+        super().save(*args, **kwargs)
+
+
+class Follow(models.Model):
     """Класс подписок пользователя."""
+
     user = models.ForeignKey(
-        MyUser, on_delete=models.CASCADE, related_name='follower'
+        CustomUser, on_delete=models.CASCADE, related_name='follower',
+        help_text='Подписчик'
     )
     following = models.ForeignKey(
-        MyUser, on_delete=models.CASCADE, related_name='following'
+        CustomUser, on_delete=models.CASCADE, related_name='following',
+        help_text='Автор'
     )
 
     class Meta:
