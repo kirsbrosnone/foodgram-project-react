20f32082591f9f709753cf2453ba89f83bbf3b2d
Кирсанов Роман <romat42ru@gmail.com>
2022-08-27 22:24:00 +0700

    Workflow 13

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,10 +1,16 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
