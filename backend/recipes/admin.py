cb5aa08f1132f9259c114e90715a947e28ad98bf
Кирсанов Роман <romat42ru@gmail.com>
2022-08-30 20:35:41 +0700

    admin page fix

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -7,32 +7,34 @@ from .models import (
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'measurement_unit')
-    search_fields = ('name')
-    list_filter = ('measurement_unit')
-    empty_value_display = ('-пусто-')
+    list_display = ('pk', 'name', 'measurement_unit', )
+    search_fields = ('name', )
+    list_filter = ('measurement_unit', )
+    empty_value_display = '-пусто-'
 
 
 @admin.register(AmountOfIngredient)
 class AmountOfIngredientAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'ingredient', 'amount')
+    list_display = ('pk', 'ingredient', 'amount', )
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'color', 'slug')
-    search_fields = ('name', 'color', 'slug')
-    list_filter = ('name')
-    empty_value_display = ('-пусто-')
+    list_display = ('pk', 'name', 'color', 'slug', )
+    search_fields = ('name', 'color', 'slug', )
+    list_filter = ('name', )
+    empty_value_display = '-пусто-'
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'pub_date', 'author', 'name', 'text', 'cooking_time')
-    readonly_fields = ('favourites')
-    list_filter = ('pub_date', 'cooking_time')
-    search_fields = ('author', 'name', 'text')
-    empty_value_display = ('-пусто-')
+    list_display = (
+        'pk', 'pub_date', 'author', 'name', 'text', 'cooking_time',
+    )
+    readonly_fields = ('favourites', )
+    list_filter = ('pub_date', 'cooking_time', )
+    search_fields = ('author', 'name', 'text', )
+    empty_value_display = '-пусто-'
 
     def favourites(self, obj):
         return obj.favourite_recipe.count()
@@ -40,15 +42,15 @@ class RecipeAdmin(admin.ModelAdmin):
 
 @admin.register(FavouriteRecipe)
 class FavouriteRecipeAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'user', 'recipe')
-    list_filter = ('user')
-    search_fields = ('recipe')
-    empty_value_display = ('-пусто-')
+    list_display = ('pk', 'user', 'recipe', )
+    list_filter = ('user', )
+    search_fields = ('recipe', )
+    empty_value_display = '-пусто-'
 
 
 @admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'user', 'recipe')
-    list_filter = ('user')
-    search_fields = ('recipe')
-    empty_value_display = ('-пусто-')
+    list_display = ('pk', 'user', 'recipe', )
+    list_filter = ('user', )
+    search_fields = ('recipe', )
+    empty_value_display = '-пусто-'
